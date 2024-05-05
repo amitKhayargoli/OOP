356630f2fecd108bcefac3421cbc74bf48df00ff
Amit Khayargoli <khayargoliamit99@gmail.com>
2024-05-05 10:20:05 +0545

    vowel

--- Week3/vowel.java
+++ Week3/vowel.java
@@ -11,27 +11,36 @@ public class vowel {
 
     System.out.println("Enter an alphabet");
     String a=scanner.nextLine();
-    
     a=a.toLowerCase();
-   
+
+    /*
     boolean isVowel=false;
-
-    String vowels[]={"a","e","i","o","u"};
-
-        for (String vowel : vowels){
-            if (vowel.equals(a)){
-                isVowel=true;
-                break;
-            }
-
-        }
-        if(isVowel){
-            System.out.println(a+" is a vowel");
-        }
-        else{
-            System.out.println(a+" is not a vowel");
+ * 
+ String vowels[]={"a","e","i","o","u"};
+ //for-each loop to iterate each element of the array of vowels
+ for (String vowel : vowels){
+     if (vowel.equals(a)){
+         isVowel=true;
+         break;
         }
         
+    }
+    if(isVowel){
+        System.out.println(a+" is a vowel");
+    }
+    else{
+        System.out.println(a+" is not a vowel");
+    }    
+}
+*/
+
+//using basic if-else statements
+if(a.equals("a")||a.equals("e")||a.equals("i")||a.equals("o")||a.equals("u")){
+    System.out.println(a+" is a vowel");
+}
+else{
+    System.out.println(a+" is a consonant");
 }
 
+  }
 }
